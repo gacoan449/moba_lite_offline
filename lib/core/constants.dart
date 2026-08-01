1ceb493c4d7093c28fc5f3e662935ee7093859cd
gacoan449 <gacoan449@gmail.com>
2026-08-01 15:09:57 +0700

    Define game constants for player and colors
    Added constants for player stats and colors.

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class GameConstants {
+  // Player Stats
+  static const double playerBaseSpeed = 160.0;
+  static const double playerBaseHp = 100.0;
+  static const double playerBaseDamage = 25.0;
+  static const double premiumLaserDamage = 9999.0;
+  static const double attackCooldown = 0.5; // Detik
+
+  // Colors (Untuk Placeholder)
+  static const Color playerColor = Colors.blueAccent;
+  static const Color enemyColor = Colors.redAccent;
+  static const Color laserColor = Colors.cyanAccent;
+  static const Color basicBulletColor = Colors.orange;
+}
